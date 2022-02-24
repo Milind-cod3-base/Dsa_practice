05cafebedf7436ff492f4165a78549c7e692366a
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-24 21:40:48 +0530

    Finding the block where element is

--- jumpSearch.py
+++ jumpSearch.py
@@ -4,4 +4,13 @@ def JumpSearch(arr,x,n):
 
     #finding block size to be jumped
     step = math.sqrt(n)
-    
+
+    #finding element in the list
+
+    prev = 0
+    while arr[int(min(step,n)-1)] < x:
+        prev = step
+        step += math.sqrt(n)
+        if prev >=n:
+            return -1
+            

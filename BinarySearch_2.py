0e6dd7b5d2c81692b597b6ac10010ec67f51d9c4
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-24 20:54:50 +0530

    function call + printing results

--- BinarySearch_2.py
+++ BinarySearch_2.py
@@ -33,3 +33,16 @@ def BinarySearchIterative(arr,l,r,x):
 arr = [2,3,4,10,40]
 
 x= 10 
+
+# Function call
+
+result = BinarySearchIterative(arr,0, len(arr)-1, x)
+
+
+if result != 1:
+    
+    print("Element is present at index % d" % result)
+
+else:
+
+    print("Element is not present in the array")

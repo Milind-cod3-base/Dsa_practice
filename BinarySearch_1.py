bf98beae5969bf481a91f7d04f90ace374e09186
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-24 18:50:43 +0530

    Creating Binary Search function - base case

---
+++ BinarySearch_1.py
@@ -0,0 +1,11 @@
+#this is to do Binary search in a list.
+
+#this is a type of recursive algorithm
+
+def BinarySearch(arr,l,r,x):   #where x is th target value and l is the leftmost element (start element) and r is the right-most element (end)
+
+    #checking the base case
+
+    if r>=1:
+        mid = l + (r-1) //2
+        

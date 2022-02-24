da4d93682276eca4d9e3f2a687edbfd6a9ce8b23
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-24 21:48:33 +0530

    jump search function created

--- jumpSearch.py
+++ jumpSearch.py
@@ -13,4 +13,21 @@ def JumpSearch(arr,x,n):
         step += math.sqrt(n)
         if prev >=n:
             return -1
-            
+
+    
+    #linear search in the block range
+
+    while arr[int(prev)] <x:
+
+        prev +=1
+
+        #if we reached next block or end, element is not present in the array
+
+        if prev == min(step,n):
+            return -1
+        
+        #if element is found
+        if arr[int(prev)]== x:
+            return prev
+
+        return -1

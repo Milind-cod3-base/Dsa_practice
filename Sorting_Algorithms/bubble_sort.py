8489d5f0c2b5548f19973f483fa3034d0f32a5d6
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-27 17:04:14 +0530

    Driver code written

--- Sorting_Algorithms/bubble_sort.py
+++ Sorting_Algorithms/bubble_sort.py
@@ -14,3 +14,12 @@ def bubbleSort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+
+#drivers code
+
+A = [64, 34, 25, 12, 22, 11, 90]
+
+x= bubbleSort(A)
+
+print(x)

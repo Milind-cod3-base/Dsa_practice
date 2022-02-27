9c5b5be72506276a2f6a89c8256d9ff87c7aa867
Milind-cod3-base <cntcod3@protonmail.com>
2022-02-27 17:01:43 +0530

    function bubble sort built

---
+++ Sorting_Algorithms/bubble_sort.py
@@ -0,0 +1,16 @@
+
+#takes two element and compares them and keep traversing further in the list
+
+#worst case scenario: reverse sorted array O(n2)
+#best case scenario: already sorted array O(n)
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+
+        for j in range(0,n-i-1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
